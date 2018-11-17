6a63f3446e48a1615d14437a94f4dcecc2846e43
paloma-vel <paloma.velazquez@gmail.com>
2018-11-17 14:26:14 -0800

    Add refresh button with no functionality

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -1,27 +1,6 @@
 var MessagesView = {
 
   $chats: $('#chats'),
-
-  // initialize: function() {
-  //   MessagesView.render();
-  //   // $('.username').on('click', function(event) {
-  //   //   console.log('here');
-  //   //   Friends.toggleStatus(event.target.innerText);
-  //   // });
-  // },
-
-  // render: function() {
-  //   App.fetch((data) => {
-  //     Messages.messages = data.results;
-  //     for (var i = 0; i < Messages.messages.length; i++) {
-  //       MessagesView.renderMessage(Messages.messages[i]);
-  //     }
-  //   });
-  // },
-  
-  // renderMessage: function(message) {
-  //   MessageView.render(message);
-  // }
   
   initialize: function() {
     App.fetch((data) => {
